1710c92908f58b8099245a76ba524eca0f51946c
ngduktrong <ninjatrong2005@gmail.com>
2025-05-02 23:49:52 +0700

    Javaproject/src/App.java

--- Javaproject/src/App.java
+++ Javaproject/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Worlddddd!");
+        System.out.println("helo tat ca cac ban da den voi java ");
     }
 }
