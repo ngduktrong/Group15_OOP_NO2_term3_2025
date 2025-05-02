b4a4c2e5b4b2081e68d813f45e59bfceb5b4a897
ngduktrong <ninjatrong2005@gmail.com>
2025-05-03 00:04:20 +0700

    sql

--- Javaproject/src/App.java
+++ Javaproject/src/App.java
@@ -1,6 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Worlddddd!");
-        System.out.println("helo tat ca cac ban da den voi java ");
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test"; // tên CSDL là test
+        String user = "root";                             // tài khoản MySQL
+        String password = "";                             // mật khẩu MySQL
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("✅ Kết nối MySQL thành công!");
+        } catch (SQLException e) {
+            System.out.println("❌ Lỗi kết nối: " + e.getMessage());
+        }
     }
-}
