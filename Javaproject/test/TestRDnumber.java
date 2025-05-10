728a7c2b19ec08b7809c1b90ae57b2cd55d85e4a
DuongManh <duonghmanh123@gmail.com>
2025-05-10 14:05:27 +0700

    RDNB

---
+++ Javaproject/test/TestRDnumber.java
@@ -0,0 +1,9 @@
+package Javaproject.test;
+import Javaproject.src.models.RDnumber;
+
+public class TestRDnumber {
+    public static void main(String[] args) {
+        int RandomNumber = RDnumber.RandNum();
+        System.out.println("Số ngẫu nhiên là: " + RandomNumber);
+    }
+}
