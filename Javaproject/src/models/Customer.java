a71f3ebefa679bf7f0d80b2cca1ae0c6bfb1a32e
DuongManh <duonghmanh123@gmail.com>
2025-05-13 12:08:33 +0700

    customer private

--- Javaproject/src/models/Customer.java
+++ Javaproject/src/models/Customer.java
@@ -1,24 +1,34 @@
-package Javaproject.src.models;
-public class Customer{
-    int makhachhang;
+package models;
+public class Customer {
+    private int makhachhang;
+    private String ten;
+    private String sodienthoai;
+    private String email;
 
-    String ten;
-
-    String  sodienthoai;
-
-    String email;
-    Customer(int makhachhang ,String ten, String sodienthoai ,String email){
-        this.makhachhang=makhachhang;
-        this.ten=ten;
-        this.sodienthoai=sodienthoai;
-        this.email=email;
+    public Customer(int makhachhang, String ten, String sodienthoai, String email) {
+        this.makhachhang = makhachhang;
+        this.ten = ten;
+        this.sodienthoai = sodienthoai;
+        this.email = email;
     }
-    public static void main(String[] args){
-        Customer KH= new Customer(123,"nguyen van hoang","0123456789","hoang@gmai.com");
-        System.out.println(KH.makhachhang);
-        System.out.println(KH.ten);
-        System.out.println(KH.sodienthoai);
-        System.out.println(KH.email);
-
+    public int getMakhachhang() {
+        return makhachhang;
     }
+
+    public String getTen() {
+        return ten;
+    }
+    public String getSodienthoai() {
+        return sodienthoai;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void hienThiThongTin() {
+        System.out.println("Ma KH: " + makhachhang);
+        System.out.println("Ten: " + ten);
+        System.out.println("SĐT: " + sodienthoai);
+        System.out.println("Email: " + email);
+    }
+
 }
