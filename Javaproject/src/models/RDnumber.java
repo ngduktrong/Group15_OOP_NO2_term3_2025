fa6fb16786c6fbbf810bd8cb2e72153d73187a9f
ngduktrong <ninjatrong2005@gmail.com>
2025-05-17 14:17:18 +0700

    delete

--- Javaproject/src/models/RDnumber.java
+++
@@ -1,9 +0,0 @@
-package Javaproject.src.models;
-import java.util.Random;
-public class RDnumber{
-    public static int RandNum(){
-        Random r = new Random();
-        int num =r.nextInt(1000);
-        return num;
-    }
-}
