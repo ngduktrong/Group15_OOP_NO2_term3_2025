4cff6623086ef3d1cb83fe8d7ea72f5f9c0e2a3c
ngduktrong <ninjatrong2005@gmail.com>
2025-05-17 15:42:36 +0700

    sqlsql

---
+++ Javaproject/src/DataBase/DataBase.java
@@ -0,0 +1,30 @@
+package DataBase;
+package com.finalproj.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Database {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/store_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456"; //
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Kết nối đến cơ sở dữ liệu
+            Connection connect = DriverManager.getConnection(URL, USER, PASSWORD);
+            return connect;
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver không được tìm thấy: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Kết nối thất bại: " + e.getMessage());
+        }
+        return null;
+    }
+}
+
