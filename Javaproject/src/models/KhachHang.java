f7af1fe8d6e16974ee8a00f36dd7bf4f0a214459
DuongManh <duonghmanh123@gmail.com>
2025-05-17 14:43:44 +0700

    push

--- Javaproject/src/models/KhachHang.java
+++ Javaproject/src/models/KhachHang.java
@@ -1,4 +1,22 @@
-package models;
+package Javaproject.src.models;
 
 public class KhachHang {
+    public int MaKhachHang;
+    public String Ten;
+    public String SoDienThoai;
+    public String Email;
+
+
+    public KhachHang( int MaKhachHang,String Ten,String SoDienThoai, String Email ){
+        this.MaKhachHang =MaKhachHang;
+        this.Ten=Ten;
+        this.SoDienThoai=SoDienThoai;
+        this.Email=Email;
+    }
+    public void hienthithongtinkhachhang(){
+        System.out.println("Ma Khach Hang:" +MaKhachHang);
+        System.out.println("Ten:"+Ten);
+        System.out.println("So Dien Thoai:"+SoDienThoai);
+        System.out.println("Email:"+Email);
+    }
 }
