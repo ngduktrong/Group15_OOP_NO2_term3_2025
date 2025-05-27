c4b5106a86ebe84e065f687899149bf08fc49100
DHManh-rambo <duonghmanh123@gmail.com>
2025-05-27 09:43:04 +0700

    new

--- src/main/java/com/group15/models/NguoiDung.java
+++ src/main/java/com/group15/models/NguoiDung.java
@@ -2,67 +2,67 @@ package com.group15.models;
 
 public class NguoiDung {
     
-    @DBTable(columnName = "MaNguoiDung", isPrimaryKey = true)
+   
     private int MaNguoiDung;
     
-    @DBTable(columnName = "HoTen")
+    
     private String HoTen;
 
-    @DBTable(columnName = "SoDienThoai")
+   
     private String SoDienThoai;
-    @DBTable(columnName = "Email")
+    
     private String Email;
-    @DBTable(columnName = "LoaiNguoiDung")
+   
     private String LoaiNguoiDung; // KhachHang hoặc NhanVien
 
    
 
     public NguoiDung(int maNguoiDung, String hoTen, String soDienThoai, String email, String loaiNguoiDung) {
-        this.MaNguoiDung = maNguoiDung;
-        this.HoTen = hoTen;
-        this.SoDienThoai = soDienThoai;
-        this.Email = email;
-        this.LoaiNguoiDung = loaiNguoiDung;
+        this.MaNguoiDung = MaNguoiDung;
+        this.HoTen = HoTen;
+        this.SoDienThoai = SoDienThoai;
+        this.Email = Email;
+        this.LoaiNguoiDung = LoaiNguoiDung;
     }
 
     public int getMaNguoiDung() {
         return MaNguoiDung;
     }
 
-    public void setMaNguoiDung(int maNguoiDung) {
-        this.MaNguoiDung = maNguoiDung;
+    public void setMaNguoiDung(int MaNguoiDung) {
+        this.MaNguoiDung = MaNguoiDung;
     }
 
     public String getHoTen() {
         return HoTen;
     }
 
-    public void setHoTen(String hoTen) {
-        this.HoTen = hoTen;
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
     }
 
     public String getSoDienThoai() {
         return SoDienThoai;
     }
 
-    public void setSoDienThoai(String soDienThoai) {
-        this.SoDienThoai = soDienThoai;
+    public void setSoDienThoai(String SoDienThoai) {
+        this.SoDienThoai = SoDienThoai;
     }
 
     public String getEmail() {
         return Email;
     }
 
-    public void setEmail(String email) {
-        this.Email = email;
+    public void setEmail(String Email) {
+        this.Email = Email;
     }
 
     public String getLoaiNguoiDung() {
         return LoaiNguoiDung;
     }
 
-    public void setLoaiNguoiDung(String loaiNguoiDung) {
-        this.LoaiNguoiDung = loaiNguoiDung;
+    public void setLoaiNguoiDung(String LoaiNguoiDung) {
+        this.LoaiNguoiDung = LoaiNguoiDung;
     }
 
     @Override
