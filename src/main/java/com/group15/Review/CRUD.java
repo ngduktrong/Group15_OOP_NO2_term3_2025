b5ee7787d0b712f8e6badc70c2318b2b5b150c0f
ngduktrong <ninjatrong2005@gmail.com>
2025-05-27 12:06:30 +0700

    commit

--- src/main/java/com/group15/Review/CRUD.java
+++
@@ -1,5 +0,0 @@
-package com.group15.Review;
-
-public class CRUD {
-    
-}
---
+++ src/main/java/com/group15/Review/GenericCRUD.java
@@ -0,0 +1,41 @@
+package com.group15.Review;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericCRUD<T> {
+    private List<T> list = new ArrayList<>();
+
+    public void add(T obj) {
+        list.add(obj);
+    }
+
+    public List<T> getAll() {
+        return list;
+    }
+
+    public boolean update(int index, T obj) {
+        if (index >= 0 && index < list.size()) {
+            list.set(index, obj);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(int index) {
+        if (index >= 0 && index < list.size()) {
+            list.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public void printAll() {
+        if (list.isEmpty()) {
+            System.out.println("Danh sách trống.");
+            return;
+        }
+        for (T obj : list) {
+            System.out.println(obj);
+        }
+    }
+}
